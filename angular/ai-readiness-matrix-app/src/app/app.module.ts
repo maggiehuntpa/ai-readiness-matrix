a742a204b16df0f86197f460c9cd367da3384f2b
Maggie Hunt <maggie.hunt@paconsulting.com>
2024-06-10 18:22:48 +0100

    changed to flask from angular

---
+++ angular/ai-readiness-matrix-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { ResultsComponent } from './components/results/results.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuizComponent,
+    ResultsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
